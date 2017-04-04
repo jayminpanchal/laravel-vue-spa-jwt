359d16b14c2a74fb599efefcb1f94b875200c7c9
Jaymin <jayminpanchal10@gmail.com>
2017-04-05 01:38:01 +0530

    created basic auth store

--- resources/assets/js/store/auth/MutationTypes.js
+++ resources/assets/js/store/auth/MutationTypes.js
@@ -1,6 +1,5 @@
 export const LOGOUT = 'LOGOUT';
-export const SAVE_TOKEN = 'SAVE_TOKEN';
-export const FETCH_USER = 'FETCH_USER';
+export const SAVE_USER = 'SAVE_USER';
+export const UPDATE_USER = 'UPDATE_USER';
 export const FETCH_USER_SUCCESS = 'FETCH_USER_SUCCESS';
 export const FETCH_USER_FAILURE = 'FETCH_USER_FAILURE';
-export const UPDATE_USER = 'UPDATE_USER';
--- resources/assets/js/store/auth/index.js
+++ resources/assets/js/store/auth/index.js
@@ -1,3 +1,5 @@
+import * as MutationTypes from './MutationTypes';
+
 const state = {
     user: {
         name: ''
@@ -5,15 +7,48 @@ const state = {
     token: ''
 };
 
-const mutations={
-
+const mutations = {
+    [MutationTypes.SAVE_USER](state, response) {
+        state.token = response.data.token;
+        state.user.name = response.data.user.name;
+    },
+    [MutationTypes.LOGOUT](state) {
+        state.token = '';
+        state.user.name = '';
+    },
+    [MutationTypes.FETCH_USER_SUCCESS](state, user) {
+        state.user.name = user.name;
+    },
+    [MutationTypes.FETCH_USER_FAILURE](state) {
+        state.user.name = '';
+        state.token = '';
+    },
+    [MutationTypes.UPDATE_USER](state, user) {
+        state.user.name = user.name;
+    }
 };
-const actions={
-
+const actions = {
+    [MutationTypes.SAVE_USER]({commit}, response) {
+        commit(MutationTypes.SAVE_USER, response);
+    },
+    [MutationTypes.LOGOUT]({commit}) {
+        commit(MutationTypes.LOGOUT);
+    },
+    [MutationTypes.FETCH_USER_SUCCESS]({commit}, user) {
+        commit(MutationTypes.FETCH_USER_SUCCESS, user);
+    },
+    [MutationTypes.FETCH_USER_FAILURE]({commit}) {
+        commit(MutationTypes.FETCH_USER_FAILURE);
+    }
 };
-const getters={
-
+const getters = {
+    authUser: state => state.user,
+    authToken: state => state.token,
+    isLoggedIn: state => state.token !== ''
 };
-export default{
-
+export default {
+    state,
+    mutations,
+    actions,
+    getters
 }
